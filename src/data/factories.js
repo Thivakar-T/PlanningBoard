26d4555b0348862801c6385eeaa542807125b739
Seetharaman GR <grsind19@gmail.com>
2021-05-02 15:55:03 +0530

    Updating the dynamic changes

---
+++ src/data/factories.js
@@ -0,0 +1,169 @@
+const factories = [
+  {
+    factoryId: "F001",
+    factoryName: "Master",
+    location: "Chennai",
+    allocations: [
+      {
+        month: "Jan",
+        orders: [],
+      },
+      {
+        month: "Feb",
+        orders: [],
+      },
+      {
+        month: "Mar",
+        orders: [],
+      },
+      {
+        month: "Apr",
+        orders: [],
+      },
+      {
+        month: "May",
+        orders: [],
+      },
+      {
+        month: "Jun",
+        orders: [],
+      },
+      {
+        month: "Jul",
+        orders: [],
+      },
+      {
+        month: "Aug",
+        orders: [],
+      },
+      {
+        month: "Sep",
+        orders: [],
+      },
+      {
+        month: "Oct",
+        orders: [],
+      },
+      {
+        month: "Nov",
+        orders: [],
+      },
+      {
+        month: "Dec",
+        orders: [],
+      },
+    ],
+  },
+  {
+    factoryId: "F002",
+    factoryName: "Gilli",
+    location: "Bangalore",
+    allocations: [
+      {
+        month: "Jan",
+        orders: [],
+      },
+      {
+        month: "Feb",
+        orders: [],
+      },
+      {
+        month: "Mar",
+        orders: [],
+      },
+      {
+        month: "Apr",
+        orders: [],
+      },
+      {
+        month: "May",
+        orders: [],
+      },
+      {
+        month: "Jun",
+        orders: [],
+      },
+      {
+        month: "Jul",
+        orders: [],
+      },
+      {
+        month: "Aug",
+        orders: [],
+      },
+      {
+        month: "Sep",
+        orders: [],
+      },
+      {
+        month: "Oct",
+        orders: [],
+      },
+      {
+        month: "Nov",
+        orders: [],
+      },
+      {
+        month: "Dec",
+        orders: [],
+      },
+    ],
+  },
+  {
+    factoryId: "F003",
+    factoryName: "Sarkar",
+    location: "Mumbai",
+    allocations: [
+      {
+        month: "Jan",
+        orders: [],
+      },
+      {
+        month: "Feb",
+        orders: [],
+      },
+      {
+        month: "Mar",
+        orders: [],
+      },
+      {
+        month: "Apr",
+        orders: [],
+      },
+      {
+        month: "May",
+        orders: [],
+      },
+      {
+        month: "Jun",
+        orders: [],
+      },
+      {
+        month: "Jul",
+        orders: [],
+      },
+      {
+        month: "Aug",
+        orders: [],
+      },
+      {
+        month: "Sep",
+        orders: [],
+      },
+      {
+        month: "Oct",
+        orders: [],
+      },
+      {
+        month: "Nov",
+        orders: [],
+      },
+      {
+        month: "Dec",
+        orders: [],
+      },
+    ],
+  },
+];
+
+export default factories;
---
+++ src/data/order.js
@@ -0,0 +1,27 @@
+const orders = [
+  {
+    orderId: "123",
+    style: "Jockey",
+    qty: 10000,
+    class: "primary",
+  },
+  {
+    orderId: "234",
+    style: "Ramraj",
+    qty: 40000,
+    class: "secondary",
+  },
+  {
+    orderId: "345",
+    style: "UsPolo",
+    qty: 30000,
+    class: "danger",
+  },
+  {
+    orderId: "456",
+    style: "Viking",
+    qty: 20000,
+    class: "success",
+  },
+];
+export default orders;
